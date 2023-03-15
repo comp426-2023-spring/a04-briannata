8beb06236697bde3ac2f98380d8f8fcdbdc4a53c
briannata <brianna.hy.ta@gmail.com>
2023-03-15 18:30:30 -0400

    Add post routes

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ app.get('/app/rpsls/play', (req, res) => {
     res.status(200).send(rpsls(req.query.shot));
 })
 
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
 app.get('/app/rps/play/:arg', (req, res) => {
     res.status(200).send(rps(req.params.arg));
 })
